77d23d2e8c2bcfd2ca5a86c6357236dbe3c22766
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-05 18:03:04 +0530

    refactor and test mood analyser

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -8,6 +8,48 @@ namespace MoodAnalyser1
 {
     internal class MoodAnalyser
     {
+        //public string message;
+
+        //public MoodAnalyser(string msg)
+        //{
+        //    this.message = msg;
+        //}
+        //public string AnalyseMood(string msg)
+        //{
+
+        //    if (msg.Length > 0)
+        //    {
+        //        message = msg;
+        //    }
+        //    if (msg.Contains("happy", StringComparison.OrdinalIgnoreCase))
+        //    {
+        //        return "happy";
+        //    }
+        //    if (msg.Contains("sad", StringComparison.OrdinalIgnoreCase))
+        //    {
+        //        return "sad";
+        //    }
+
+        //    return null;
+        //}
+        //public string AnalyseAnyMood(string msg)
+        //{
+
+        //    if (msg.Length > 0)
+        //    {
+        //        message = msg;
+        //    }
+
+        //    if (msg.Contains("sad", StringComparison.OrdinalIgnoreCase))
+        //    {
+        //        return "sad";
+        //    }
+        //    else
+        //    {
+        //        return "happy";
+        //    }
+        //    return null;
+        //}
         public string message;
 
         public MoodAnalyser(string msg)
@@ -29,7 +71,6 @@ namespace MoodAnalyser1
             {
                 return "sad";
             }
-
             return null;
         }
         public string AnalyseAnyMood(string msg)
@@ -50,6 +91,9 @@ namespace MoodAnalyser1
             }
             return null;
         }
+
+
+
 
     }
 }
