be95502cf1ac7c91e6aac320352bc956af2a1d4a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-05 18:01:59 +0530

    handle mood exception

--- MoodAnalyse.cs
+++ MoodAnalyse.cs
@@ -8,56 +8,80 @@ namespace MoodAnalyser1
 {
     internal class MoodAnalyse
     {
+        //public string Info;
+        //public MoodAnalyse(string info)
+        //{
+        //    this.Info = info;
+        //}
+        //public string AnalyseEmptyMood()
+        //{
+        //    try
+        //    {
+        //        if (this.Info.Equals(string.Empty))
+        //        {
+        //            throw new CustomException(CustomException.Mood.Empty, "Its Empty");
+        //        }
+        //    }
+
+
+        //    catch (NullReferenceException)
+        //    {
+        //        throw new CustomException(CustomException.Mood.Empty, "Its Empty");
+        //    }
+        //    catch (CustomException ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return ex.Message;
+        //    }
+        //    return null;
+        //}
+        //public string AnalyseNullMood()
+        //{
+
+
+        //    try
+        //    {
+        //        if (this.Info == null)
+        //        {
+        //            throw new CustomException(CustomException.Mood.Null, "Its Null");
+        //        }
+        //    }
+        //    catch (NullReferenceException)
+        //    {
+        //        throw new CustomException(CustomException.Mood.Null, "Its Null");
+        //    }
+        //    catch (CustomException ex1)
+        //    {
+        //        Console.WriteLine(ex1.Message);
+        //        return ex1.Message;
+        //    }
+        //    return null;
+        //}
+
+        //public string AnalyseMood()
+        //{
+        //    try
+        //    {
+        //        if (Info.Contains("sad", StringComparison.OrdinalIgnoreCase))
+        //        {
+        //            return "sad";
+        //        }
+        //        else
+        //        {
+        //            return "happy";
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        return "happy";
+        //    }
+        //}
+
         public string Info;
         public MoodAnalyse(string info)
         {
             this.Info = info;
         }
-        public string AnalyseEmptyMood()
-        {
-            try
-            {
-                if (this.Info.Equals(string.Empty))
-                {
-                    throw new CustomException(CustomException.Mood.Empty, "Its Empty");
-                }
-            }
-
-
-            catch (NullReferenceException)
-            {
-                throw new CustomException(CustomException.Mood.Empty, "Its Empty");
-            }
-            catch (CustomException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return ex.Message;
-            }
-            return null;
-        }
-        public string AnalyseNullMood()
-        {
-
-
-            try
-            {
-                if (this.Info == null)
-                {
-                    throw new CustomException(CustomException.Mood.Null, "Its Null");
-                }
-            }
-            catch (NullReferenceException)
-            {
-                throw new CustomException(CustomException.Mood.Null, "Its Null");
-            }
-            catch (CustomException ex1)
-            {
-                Console.WriteLine(ex1.Message);
-                return ex1.Message;
-            }
-            return null;
-        }
-
         public string AnalyseMood()
         {
             try
@@ -68,6 +92,7 @@ namespace MoodAnalyser1
                 }
                 else
                 {
+
                     return "happy";
                 }
             }
@@ -75,6 +100,8 @@ namespace MoodAnalyser1
             {
                 return "happy";
             }
+
+
         }
     }
 }
